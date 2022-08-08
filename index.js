1bce14d6f568007e42c8a7b3fde9791e9ee26047
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-08 14:12:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name = "Aki"){
+    return `Hi, my name is ${
+        name
+    }.`
+}
+function introductionWithLanguage(name="Aki",languge="Ember.js"){
+    return `Hi, my name is ${name} and I am learning to program in ${languge}.`
+}
+function introductionWithLanguageOptional(Aki,languge="JavaScript"){
+    return `Hi, my name is ${Aki} and I am learning to program in ${languge}.`
+}
